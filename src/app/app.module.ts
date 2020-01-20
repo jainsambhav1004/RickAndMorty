ffd89edd94e036f4a5bc6214f162a27e78283921
Sambhav Jain <sambhavjain@Sambhav-MacBook-Air.local>
2020-01-21 00:53:40 +0530

    Third Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +15,12 @@ import { DashboardServiceService } from './dashboard/dashboard-service.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
+  entryComponents: [],
   providers: [
     DashboardServiceService
   ],
